e3c7a9881712948ba7d4aaa932bacfa8917a166c
Nate Rodrigues <nathan.rodrigues@live.com>
2020-08-29 08:32:56 +1000

    Initial Commit

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -91,4 +91,89 @@ switch(job) {
 }
 
 //  FALSY VALUES: Undefined, null, 0, '', NaN will be evaluated to false
+//  TRUTHY VALUES: NOT falsy values
+// BEST PRACTICE IS USING === IN LOGIC STATEMENTS
+
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+*/
+
+var teamJohn, teamMike, teamMary;
+
+teamJohn = (89+120+103)/3;
+teamMike = (116+94+123)/3;
+teamMary = (97+134+105)/3;
+
+console.log(teamJohn,teamMike,teamMary);
+
+if (teamJohn > teamMike && teamJohn > teamMary) {
+    console.log('John\'s team is the winner with '+ teamJohn+' points!');
+} else if (teamMike > teamJohn && teamMike > teamMary) {
+    console.log('Mike\'s team is the winner with '+ teamMike+' points!');
+} else if (teamMary > teamMike && teamMary > teamJohn) {
+    console.log('Mary\'s team is the winner with '+ teamMary+' points!');
+} else {
+    console.log ('There was no winner, all teams drawed.');
+}
+
+
+console.log('Function Section');
+
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+
+console.log(ageJohn,ageMike,ageJane);
+
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    console.log(firstName + ' retires in ' + retirement + ' years.');
+}
+
+yearsUntilRetirement(1994,'Nate');
+
+
+/* Function Statements and Expression */
+
+// Function Declaration Example
+
+// function whatDoYouDo(job,firstName) {};
+
+// Function Expression. Expressions always result in a value.
+
+var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code'; // We don't include the break when we hit return.
+        case 'driver':
+            return firstName + ' drives a cab in Sydney';
+        case 'designer':
+            return firstName + ' designs good websites';
+        default:
+            return firstName + ' does something else';
+    }
+}
+
+console.log(whatDoYouDo('teacher','John'));
+console.log(whatDoYouDo('designer','Jane'));
+console.log(whatDoYouDo('retired','Mark'));
+
 
